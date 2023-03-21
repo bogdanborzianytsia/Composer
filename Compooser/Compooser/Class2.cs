c3f62e419abdcdb869002948666e84ec657030cc
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-21 18:41:04 +0200

    Add files via upload

---
+++ Compooser/Compooser/Class2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compooser
+{
+    abstract class PizzaComponent
+    {
+        public abstract void Display();
+    }
+
+    class Topping : PizzaComponent
+    {
+        private string name;
+
+        public Topping(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"- {name}");
+        }
+    }
+
+    class Pizza : PizzaComponent
+    {
+        private string name;
+        private List<PizzaComponent> toppings = new List<PizzaComponent>();
+
+        public Pizza(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddTopping(PizzaComponent topping)
+        {
+            toppings.Add(topping);
+        }
+
+        public void RemoveTopping(PizzaComponent topping)
+        {
+            toppings.Remove(topping);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Pizza: {name}");
+            Console.WriteLine("Toppings:");
+
+            foreach (PizzaComponent topping in toppings)
+            {
+                topping.Display();
+            }
+
+            Console.WriteLine();
+        }
+    }
+    class Client
+    {
+        public void ClientCode(PizzaComponent component)
+        {
+            component.Display();
+        }
+    }
+}
---
+++ Compooser/Compooser/Program.cs
@@ -0,0 +1,13 @@
+﻿using  Compooser;
+Pizza pepperoniPizza = new Pizza("Pepperoni");
+Topping pepperoniTopping = new Topping("Pepperoni");
+Topping cheeseTopping = new Topping("Cheese");
+
+pepperoniPizza.AddTopping(pepperoniTopping);
+pepperoniPizza.AddTopping(cheeseTopping);
+
+Client client = new Client();
+client.ClientCode(pepperoniPizza);
+
+pepperoniPizza.RemoveTopping(cheeseTopping);
+client.ClientCode(pepperoniPizza);
